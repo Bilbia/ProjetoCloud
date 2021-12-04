353247fbf505f6175e2cff08146309e9c26017c5
Bilbia <biamcs2000@gmail.com>
2021-12-03 22:37:38 -0300

    arrumando script requests

--- requests_script.py
+++ requests_script.py
@@ -4,6 +4,7 @@ print("="*10)
 print("Insira o DNS name do seu Load Balancer: ")
 url = input("DNS: ")
 
+print("="*10)
 print("Insira G para GET")
 print("Insira P para POST")
 req = input("Request: ")
@@ -13,6 +14,7 @@ if(req == "G" or req == "g"):
     print(res.json())
 
 elif(req == "P" or req == "p"):
+    print("="*10)
     title = input("Insira o título: ") 
     year = input("Insira o ano: ")
     mon = input("Insira o mês: ")
@@ -21,17 +23,22 @@ elif(req == "P" or req == "p"):
     min = input("Insira o minuto: ")
     desc = input("Insira uma descrição: ")
 
-    data = f"{year}-{mon}-{day}T{hour}:{min}"
+    date = f"{year}-{mon}-{day}T{hour}:{min}"
+
+    print("="*10)
     print(f"Tarefa criada com título {title}")
-    print(f"Data de conclusão: {data}")
+    print(f"Data de conclusão: {date}")
     print(f"Descrição: {desc}")
 
+    print("="*10)
     send = input("Insira S para enviar ou N para cancelar: ")
     if(send == "S" or send == "s"):
         res = requests.post(url, data={
             "title": title,
-            "pub_date": data,
+            "pub_date": date,
             "description": desc
         })
 
         print(res.json())
+    else:
+        quit()
